f2ece099f4d189a0cb45865a0c1ed2b61c2070ed
cadfrunze <marius1890@yahoo.com>
2023-02-04 16:46:46 +0200

    new

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import turtle
 import pandas as pd
 
-from test import coordonate
 screen = turtle.Screen()
 screen.bgcolor('black')
 screen.title('Joc interactiv cu enumerarea tuturor judetelor din Romania')
@@ -9,25 +8,9 @@ screen.setup(width=1050, height=1000)
 imagine = 'Romania_counties_blank_big.gif'
 screen.addshape(imagine)
 turtle.shape(imagine)
-screen.onscreenclick(coordonate)
-screen.tracer(0)
-
-new_elem = turtle.Turtle()
-new_elem.hideturtle()
-new_elem.penup()
-new_elem.shapesize(5)
-new_elem.goto(x=-400, y=300)
-
-dt = pd.read_csv('judetele_final.csv')
-dt = dt.judet
-dt = dt.to_list()
 
 
-for elem in dt:
-    new_elem.write(arg=f"{elem}", font=('Arial', 20, 'normal'))
-    input('enter')
-    new_elem.clear()
-    screen.update()
+
 
 
 
--- test.py
+++ test.py
@@ -2,12 +2,12 @@ import pandas as pd
 
 lista_coordonate = []
 
-def coordonate(x, y):
-    coor = (x, y)
-    lista_coordonate.append(coor)
-    with open('text.txt', 'a') as file:
-        file.write(str(lista_coordonate[-1]))
-        file.write('\n')
+# def coordonate(x, y):
+#     coor = (x, y)
+#     lista_coordonate.append(coor)
+#     with open('text.txt', 'a') as file:
+#         file.write(str(lista_coordonate[-1]))
+#         file.write('\n')
 
 
 
