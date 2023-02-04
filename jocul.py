abfcf60b5265b8dd864c70fc7d54574e703e18f6
cadfrunze <78301491+cadfrunze@users.noreply.github.com>
2023-02-05 01:02:34 +0200

    update

---
+++ jocul.py
@@ -0,0 +1,63 @@
+import turtle
+import pandas as pd
+import random
+from tkinter import messagebox
+import time
+
+COLORS = ['red', 'green', 'blue', 'brown', 'purple', 'black']
+dt = pd.read_csv('map_Romania.csv')
+
+
+# raspuns = dt[dt.judet == 'Alba']
+# cor = (int(raspuns.x), int(raspuns.y))
+# print(cor)
+
+class Robotzelu():
+    """
+    Creearea schemei a jocului, include:
+    1. Fereastra-box pt a raspunde,
+    2. Verificare raspuns,
+    3. Afisare nume judet daca raspunsul este corect, pe desen conform coordonatelor
+    """
+
+    def __init__(self):
+        self.obiect = turtle.Turtle()
+        self.ecran = turtle.Screen()
+        self.obiect.penup()
+        self.obiect.hideturtle()
+        self.ver_raspuns = []
+        self.greseli = 0
+
+    def raspunde(self):
+        self.raspuns = self.ecran.textinput(title='Scrie un judet din Ro', prompt='Raspunde aici').title()
+        self.verificare_raspuns()
+
+    def verificare_raspuns(self):
+        self.verificare_cuvant()
+        while self.raspuns in self.ver_raspuns:
+            messagebox.showinfo(title=f'{self.raspuns}', message='Ai mai scris acest judet')
+            self.raspunde()
+        else:
+            if self.raspuns in dt.judet.to_list():
+                afirmatie = dt[dt.judet == self.raspuns]
+                coor = (int(afirmatie.x), int(afirmatie.y))
+                messagebox.showinfo(title='Validat!', message='Raspuns corect!')
+                raspuns_corect = afirmatie.judet.to_list()
+                time.sleep(1)
+                self.obiect.color(random.choice(COLORS))
+                self.obiect.goto(coor)
+                self.obiect.write(arg=f'{raspuns_corect[0]}', font=('Arial', 10, 'normal'))
+                self.ver_raspuns.append(raspuns_corect[0])
+            else:
+                messagebox.showerror(title='Gresit!', message='Raspuns incorect!')
+                self.greseli += 1
+
+    def verificare_cuvant(self):
+        if ' - ' in self.raspuns:
+            self.raspuns = self.raspuns.replace(' - ', '-')
+        elif ' -' in self.raspuns:
+            self.raspuns = self.raspuns.replace(' -', '-')
+        elif '- ' in self.raspuns:
+            self.raspuns = self.raspuns.replace('- ', '-')
+        elif ' ' in self.raspuns:
+            self.raspuns = self.raspuns.replace(' ', '-')
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle
+from jocul import Robotzelu
 import pandas as pd
 
 screen = turtle.Screen()
@@ -8,8 +9,15 @@ screen.setup(width=1050, height=1000)
 imagine = 'Romania_counties_blank_big.gif'
 screen.addshape(imagine)
 turtle.shape(imagine)
+screen.tracer(0)
 
 
+robotelu = Robotzelu()
+joc = True
+
+while joc:
+    robotelu.raspunde()
+    screen.update()
 
 
 
